5745f294537fcbbae9a6c8c7ad19f545bcc55275
Andrewtho5942 <andrewtho5942@gmail.com>
2025-08-07 18:28:19 -0400

    remove tflite patching temporarily

--- react-native.config.js
+++ react-native.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   assets: ['./src/models'],
-  dependencies: {
-    'react-native-tflite': {
-      platforms: {
-        ios: {
-          podspecPath: 'node_modules/react-native-tflite/react-native-tflite.podspec',
-        },
-      },
-    },
-  },
+  
 };
