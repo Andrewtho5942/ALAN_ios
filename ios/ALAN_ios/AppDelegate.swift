9992ab6a14f3561759ef4d1915a2801939a816a1
Andrewtho5942 <andrewtho5942@gmail.com>
2025-08-07 18:49:52 -0400

    update appdelegate

--- ios/ALAN_ios/AppDelegate.swift
+++ ios/ALAN_ios/AppDelegate.swift
@@ -9,16 +9,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
     let bridge = RCTBridge(delegate: self, launchOptions: launchOptions)
     let rootView = RCTRootView(bridge: bridge!, moduleName: "ALAN_ios", initialProperties: nil)
 
-    let rootViewController = UIViewController()
-    rootViewController.view = rootView
-
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = rootViewController
-    window?.makeKeyAndVisible()
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    let rootVC = UIViewController()
+    rootVC.view = rootView
+    self.window?.rootViewController = rootVC
+    self.window?.makeKeyAndVisible()
 
     return true
   }
@@ -27,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 extension AppDelegate: RCTBridgeDelegate {
   func sourceURL(for bridge: RCTBridge!) -> URL! {
 #if DEBUG
-    return URL(string: "http://100.68.78.107:8081/index.bundle?platform=ios&dev=true")
+    return URL(string: "http://localhost:8081/index.bundle?platform=ios&dev=true")
 #else
     return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
