388857d4c9c703a397b426a83ae16c257d7060d6
Andrewtho5942 <andrewtho5942@gmail.com>
2025-08-07 19:53:42 -0400

    update appdelegate again

--- ios/ALAN_ios/AppDelegate.swift
+++ ios/ALAN_ios/AppDelegate.swift
@@ -4,17 +4,30 @@ import React
 
 @main
 class AppDelegate: RCTAppDelegate {
+
+  private let RNModuleName = "ALAN_ios"
+
   override init() {
     super.init()
-    self.moduleName = "ALAN_ios"
-    self.initialProps = [:]
+    // Force classic (legacy) UIManager / bridge behavior
+    // This ensures older native modules and view managers register as expected.
+    self.moduleName = RNModuleName
+    self.initialProps = nil
     self.fabricEnabled = false
     self.concurrentRootEnabled = false
   }
 
+  // Keep the standard delegate behavior from RCTAppDelegate.
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
   override func sourceURL(for bridge: RCTBridge!) -> URL! {
 #if DEBUG
-    return URL(string: " http://100.68.78.107:8081/index.bundle?platform=ios&dev=true")
+    return URL(string: "http://100.68.78.107:8081/index.bundle?platform=ios&dev=true")
 #else
     return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
