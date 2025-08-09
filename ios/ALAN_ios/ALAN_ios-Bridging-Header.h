59f620c00899f28b31db3e2c4dc463e821b96361
Andrewtho5942 <andrewtho5942@gmail.com>
2025-08-09 11:51:19 -0400

    add bridging header

---
+++ ios/ALAN_ios/ALAN_ios-Bridging-Header.h
@@ -0,0 +1,6 @@
+// ALAN_ios-Bridging-Header.h
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <React/RCTAppDelegate.h>
+#import <React/RCTLinkingManager.h>
