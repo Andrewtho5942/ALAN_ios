83af2fb7963127591853e3d9ea6932aa6638f108
Andrewtho5942 <andrewtho5942@gmail.com>
2025-08-09 13:44:42 -0400

    remove use_frameworks!

--- ios/ALAN_ios/ALAN_ios-Bridging-Header.h
+++ ios/ALAN_ios/ALAN_ios-Bridging-Header.h
@@ -1,2 +1,5 @@
-// ALAN_ios-Bridging-Header.h
-#import <React/React.h>
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <React/RCTAppDelegate.h>
+#import <React/RCTLinkingManager.h>
