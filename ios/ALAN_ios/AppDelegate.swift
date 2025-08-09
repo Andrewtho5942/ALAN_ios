870787d582c31854d6a8de3bd95ecc9e2477874d
Andrewtho5942 <andrewtho5942@gmail.com>
2025-08-09 15:34:36 -0400

    ensure new_arch is disabled

--- ios/ALAN_ios/AppDelegate.swift
+++ ios/ALAN_ios/AppDelegate.swift
@@ -8,12 +8,12 @@ class AppDelegate: RCTAppDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   override func sourceURL(for bridge: RCTBridge!) -> URL! {
     #if DEBUG
-      return URL(string: "http://100.68.78.107:8081/index.bundle?platform=ios&dev=true")
+      return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
     #else
       return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
     #endif
