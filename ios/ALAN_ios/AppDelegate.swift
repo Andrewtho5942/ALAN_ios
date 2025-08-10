5b4894a2c3a2a62f911eec17654de8e31d35796b
Andrewtho5942 <andrewtho5942@gmail.com>
2025-08-10 11:41:30 -0400

    revert app delegate

--- ios/ALAN_ios/AppDelegate.swift
+++ ios/ALAN_ios/AppDelegate.swift
@@ -1,26 +1,52 @@
-import React
 import UIKit
+import React
+import React_RCTAppDelegate
+import ReactAppDependencyProvider
 
 @main
-class AppDelegate: RCTAppDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
 
-  override func application(
+  var reactNativeDelegate: ReactNativeDelegate?
+  var reactNativeFactory: RCTReactNativeFactory?
+
+  func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    let delegate = ReactNativeDelegate()
+    let factory = RCTReactNativeFactory(delegate: delegate)
+    delegate.dependencyProvider = RCTAppDependencyProvider()
+
+    reactNativeDelegate = delegate
+    reactNativeFactory = factory
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+
+    factory.startReactNative(
+      withModuleName: "ALAN_ios",
+      in: window,
+      launchOptions: launchOptions
+    )
+
+    return true
+  }
+}
+
+class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
+  override func sourceURL(for bridge: RCTBridge) -> URL? {
+    self.bundleURL()
   }
 
-  override func sourceURL(for bridge: RCTBridge!) -> URL! {
-    #if DEBUG
-      return URL(string: "http://100.68.78.107:8081/index.bundle?platform=ios&dev=true")
-      //RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-    #else
-      return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-    #endif
-  }
-  
-  override func moduleName() -> String! {
-    return "ALAN_ios" 
+  override func bundleURL() -> URL? {
+#if DEBUG
+    // Hardcode the metro connection URL
+    
+    
+    return URL(string: "http://100.68.78.107:8081/index.bundle?platform=ios&dev=true")
+    //RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
   }
 }
