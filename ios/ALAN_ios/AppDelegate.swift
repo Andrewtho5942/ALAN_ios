dba58daa70d0ed4b694a278f96cc98aa20426140
Andrewtho5942 <andrewtho5942@gmail.com>
2025-08-10 11:27:25 -0400

    use old arch

--- ios/ALAN_ios/AppDelegate.swift
+++ ios/ALAN_ios/AppDelegate.swift
@@ -1,50 +1,26 @@
-import UIKit
 import React
-import React_RCTAppDelegate
-import ReactAppDependencyProvider
+import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
+class AppDelegate: RCTAppDelegate {
 
-  var reactNativeDelegate: ReactNativeDelegate?
-  var reactNativeFactory: RCTReactNativeFactory?
-
-  func application(
+  override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let delegate = ReactNativeDelegate()
-    let factory = RCTReactNativeFactory(delegate: delegate)
-    delegate.dependencyProvider = RCTAppDependencyProvider()
-
-    reactNativeDelegate = delegate
-    reactNativeFactory = factory
-
-    window = UIWindow(frame: UIScreen.main.bounds)
-
-    factory.startReactNative(
-      withModuleName: "ALAN_ios",
-      in: window,
-      launchOptions: launchOptions
-    )
-
-    return true
-  }
-}
-
-class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
-  override func sourceURL(for bridge: RCTBridge) -> URL? {
-    self.bundleURL()
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  override func bundleURL() -> URL? {
-#if DEBUG
-    // Hardcode the metro connection URL
-    return URL(string: "http://100.68.78.107:8081/index.bundle?platform=ios&dev=true")
-    //RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
+  override func sourceURL(for bridge: RCTBridge!) -> URL! {
+    #if DEBUG
+      return URL(string: "http://100.68.78.107:8081/index.bundle?platform=ios&dev=true")
+      //RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    #else
+      return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    #endif
+  }
+  
+  override func moduleName() -> String! {
+    return "ALAN_ios" 
   }
 }
